fc618fccd9a1728843b258341185288d98b950ef
cowabunga <d_roe@hotmail.co.uk>
2026-04-21 14:27:10 +0100

    Refactor player data processing: replace inline column transformations with a dedicated function

--- replacing_pogba.py
+++ replacing_pogba.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import polars as pl
 from get_players_data import get_players_data
+from transform_columns_to_float import transform_columns_to_float
 
 def main():
     FILE_PATH = r"C:\Users\d_roe\Documents\VS Code Projects\Portfolio\DE-2026-004\players_20220522.html"
@@ -28,26 +29,7 @@ def main():
         "Fls",
     ]
 
-    players_df = players_df.with_columns(
-        (pl.col("Mins").str.replace(",", "").cast(pl.Float64).alias("Mins - float")),
-        (pl.col("Hdrs A").cast(pl.Float64)).alias("Hdrs A - float"),
-        (pl.col("Clear").cast(pl.Float64)).alias("Clear - float"),
-        (pl.col("Cr A").cast(pl.Float64)).alias("Cr A - float"),
-        (pl.col("Drb").cast(pl.Float64)).alias("Drb - float"),
-        (pl.col("FA").cast(pl.Float64)).alias("FA - float"),
-        (pl.col("Itc").cast(pl.Float64)).alias("Itc - float"),
-        (pl.col("Pas A").str.replace(",", "").cast(pl.Float64)).alias("Pas A - float"),
-        (pl.col("Ps C").str.replace(",", "").cast(pl.Float64)).alias("Ps C - float"),
-        (pl.col("Shots").cast(pl.Float64)).alias("Shots - float"),
-        (pl.col("Pens").cast(pl.Float64)).alias("Pens - float"),
-        (pl.col("Tck W").cast(pl.Float64)).alias("Tck W - float"),
-        (pl.col("Tck R").str.replace("%", "").cast(pl.Float64) / 100).alias(
-            "Tck R - float"
-        ),
-        (pl.col("Yel").cast(pl.Float64)).alias("Yel - float"),
-        (pl.col("Red").cast(pl.Float64)).alias("Red - float"),
-        (pl.col("Fls").cast(pl.Float64)).alias("Fls - float"),
-    )
+    players_df = transform_columns_to_float(players_df)
 
     players_df = players_df.with_columns(
         (pl.col("Tck W - float") / pl.col("Tck R - float")).alias("Tck A")
