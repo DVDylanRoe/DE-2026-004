b177c7f25345381ae58743e2715d6a07269bc312
cowabunga <d_roe@hotmail.co.uk>
2026-04-23 20:59:01 +0100

    Refactor transformation functions: streamline column handling and update test cases for consistency

--- main.py
+++ main.py
@@ -26,24 +26,23 @@ def get_players_data(file_path: str) -> pl.DataFrame:
 
 def transform_columns_to_float(df: pl.DataFrame) -> pl.DataFrame:
     df = df.with_columns(
-        (pl.col("Mins").str.replace(",", "").cast(pl.Float64).alias("Mins - float")),
-        (pl.col("Hdrs A").cast(pl.Float64)).alias("Hdrs A - float"),
-        (pl.col("Clear").cast(pl.Float64)).alias("Clear - float"),
-        (pl.col("Cr A").cast(pl.Float64)).alias("Cr A - float"),
-        (pl.col("Drb").cast(pl.Float64)).alias("Drb - float"),
-        (pl.col("FA").cast(pl.Float64)).alias("FA - float"),
-        (pl.col("Itc").cast(pl.Float64)).alias("Itc - float"),
-        (pl.col("Pas A").str.replace(",", "").cast(pl.Float64)).alias("Pas A - float"),
-        (pl.col("Ps C").str.replace(",", "").cast(pl.Float64)).alias("Ps C - float"),
-        (pl.col("Shots").cast(pl.Float64)).alias("Shots - float"),
-        (pl.col("Pens").cast(pl.Float64)).alias("Pens - float"),
-        (pl.col("Tck W").cast(pl.Float64)).alias("Tck W - float"),
-        (pl.col("Tck R").str.replace("%", "").cast(pl.Float64) / 100).alias(
-            "Tck R - float"
-        ),
-        (pl.col("Yel").cast(pl.Float64)).alias("Yel - float"),
-        (pl.col("Red").cast(pl.Float64)).alias("Red - float"),
-        (pl.col("Fls").cast(pl.Float64)).alias("Fls - float"),
+        (pl.col("Mins").str.replace(",", "").cast(pl.Float64)),
+        (pl.col("Hdrs A").cast(pl.Float64)),
+        (pl.col("Clear").cast(pl.Float64)),
+        (pl.col("Cr A").cast(pl.Float64)),
+        (pl.col("Drb").cast(pl.Float64)),
+        (pl.col("FA").cast(pl.Float64)),
+        (pl.col("Itc").cast(pl.Float64)),
+        (pl.col("Pas A").str.replace(",", "").cast(pl.Float64)),
+        (pl.col("Ps C").str.replace(",", "").cast(pl.Float64)),
+        (pl.col("Shots").cast(pl.Float64)),
+        (pl.col("Pens").cast(pl.Float64)),
+        (pl.col("Tck W").cast(pl.Float64)),
+        (pl.col("Tck R").str.replace("%", "").cast(pl.Float64) / 100),
+        (pl.col("Yel").cast(pl.Float64)),
+        (pl.col("Red").cast(pl.Float64)),
+        (pl.col("Fls").cast(pl.Float64)),
+        (pl.col("CCC").cast(pl.Float64)).alias("CCC"),
     )
 
     return df
@@ -51,44 +50,44 @@ def transform_columns_to_float(df: pl.DataFrame) -> pl.DataFrame:
 
 def transform_custom_columns(df: pl.DataFrame) -> pl.DataFrame:
     df = df.with_columns(
-        (pl.col("Tck W - float") / pl.col("Tck R - float")).alias("Tck A")
+        (pl.col("Tck W") / pl.col("Tck R")).alias("Tck A")
     )
 
     df = df.with_columns(
-        (pl.col("Shots - float") - pl.col("Pens - float")).alias("Non-Penalty Shots")
+        (pl.col("Shots") - pl.col("Pens")).alias("Non Penalty Shots")
     )
 
-    df = df.with_columns((pl.col("Mins - float") / 90).alias("90s"))
+    df = df.with_columns((pl.col("Mins") / 90).alias("90s"))
 
     return df
 
 
 def transform_per90_columns(df: pl.DataFrame) -> pl.DataFrame:
     df = df.with_columns(
-        (pl.col("Hdrs A - float") / pl.col("90s")).alias("Hdrs A per 90"),
-        (pl.col("Clear - float") / pl.col("90s")).alias("Clear per 90"),
-        (pl.col("Cr A - float") / pl.col("90s")).alias("Cr A per 90"),
-        (pl.col("Drb - float") / pl.col("90s")).alias("Drb per 90"),
-        (pl.col("FA - float") / pl.col("90s")).alias("FA per 90"),
-        (pl.col("Itc - float") / pl.col("90s")).alias("Itc per 90"),
-        (pl.col("Pas A - float") / pl.col("90s")).alias("Pas A per 90"),
-        (pl.col("Ps C - float") / pl.col("90s")).alias("Ps C per 90"),
-        (pl.col("Non-Penalty Shots") / pl.col("90s")).alias("Non-Penalty Shots per 90"),
+        (pl.col("Hdrs A") / pl.col("90s")).alias("Hdrs A per 90"),
+        (pl.col("Clear") / pl.col("90s")).alias("Clear per 90"),
+        (pl.col("Cr A") / pl.col("90s")).alias("Cr A per 90"),
+        (pl.col("Drb") / pl.col("90s")).alias("Drb per 90"),
+        (pl.col("FA") / pl.col("90s")).alias("FA per 90"),
+        (pl.col("Itc") / pl.col("90s")).alias("Itc per 90"),
+        (pl.col("Pas A") / pl.col("90s")).alias("Pas A per 90"),
+        (pl.col("Ps C") / pl.col("90s")).alias("Ps C per 90"),
+        (pl.col("Non Penalty Shots") / pl.col("90s")).alias("Non Penalty Shots per 90"),
         (pl.col("Tck A") / pl.col("90s")).alias("Tck A per 90"),
-        (pl.col("Yel - float") / pl.col("90s")).alias("Yel per 90"),
-        (pl.col("Red - float") / pl.col("90s")).alias("Red per 90"),
-        (pl.col("Fls - float") / pl.col("90s")).alias("Fls per 90"),
+        (pl.col("Yel") / pl.col("90s")).alias("Yel per 90"),
+        (pl.col("Red") / pl.col("90s")).alias("Red per 90"),
+        (pl.col("Fls") / pl.col("90s")).alias("Fls per 90"),
     )
 
     return df
 
 
-def transform_Z_columns(df: pl.DataFrame, PER_NINETY_COLUMNS: tuple) -> pl.DataFrame:
+def transform_Z_columns(df: pl.DataFrame, per_ninety_columns: tuple) -> pl.DataFrame:
 
     df = df.with_columns(
         [
-            ((pl.col(column) - pl.mean(column)) / pl.std(column)).alias(column + " - Z")
-            for column in PER_NINETY_COLUMNS
+            ((pl.col(column) - pl.mean(column)) / pl.std(column)).alias(column + " Z")
+            for column in per_ninety_columns
         ]
     )
 
@@ -96,9 +95,9 @@ def transform_Z_columns(df: pl.DataFrame, PER_NINETY_COLUMNS: tuple) -> pl.DataF
 
 
 def main():
-    FILE_PATH = r"C:\Users\d_roe\Documents\VS Code Projects\Portfolio\DE-2026-004\players_20220522.html"
+    file_path = r"C:\Users\d_roe\Documents\VS Code Projects\Portfolio\DE-2026-004\players_20220522.html"
 
-    players_df = get_players_data(FILE_PATH)
+    players_df = get_players_data(file_path)
 
     players_df.write_csv("players-raw.csv")
 
@@ -108,20 +107,20 @@ def main():
 
     players_df = transform_per90_columns(players_df)
 
-    PER_NINETY_COLUMNS = (
+    per_ninety_columns = (
         "Hdrs A per 90",
         "Clear per 90",
         "Cr A per 90",
         "Drb per 90",
         "Itc per 90",
         "Pas A per 90",
-        "Non-Penalty Shots per 90",
+        "Non Penalty Shots per 90",
         "Tck A per 90",
     )
 
-    players_df = transform_Z_columns(players_df, PER_NINETY_COLUMNS)
+    players_df = transform_Z_columns(players_df, per_ninety_columns)
 
-    zscore_feature_columns = [column + " - Z" for column in PER_NINETY_COLUMNS]
+    zscore_feature_columns = [column + " Z" for column in per_ninety_columns]
 
     # calculate cos sim#
 
@@ -139,7 +138,7 @@ def main():
 
     players_df = players_df.with_columns(
         [
-            pl.Series("Pogba Similarity (Cosine)", cosine_similarity_scores_scaled.tolist()),
+            pl.Series("Pogba Similarity", cosine_similarity_scores_scaled.tolist()),
         ]
     )
     # calculate cos sim#
@@ -149,12 +148,8 @@ def main():
     # calculate ccr and pcr#
 
     players_df = players_df.with_columns(
-        (pl.col("CCC").cast(pl.Float64)).alias("CCC - float"),
-    )
-
-    players_df = players_df.with_columns(
-        (pl.col("CCC - float") / pl.col("Ps C - float")).alias("Chance Creation Rate"),
-        (pl.col("Ps C - float") / pl.col("Pas A - float")).alias(
+        (pl.col("CCC") / pl.col("Ps C")).alias("Chance Creation Rate"),
+        (pl.col("Ps C") / pl.col("Pas A")).alias(
             "Pass Completion Rate"
         ),
     )
@@ -165,7 +160,7 @@ def main():
 
     # filter for similar players#
 
-    midfielders_df = players_df.filter(pl.col("Pogba Similarity (Cosine)") >= 90)
+    midfielders_df = players_df.filter(pl.col("Pogba Similarity") >= 90)
 
     # filter for similar players#
 
@@ -183,7 +178,7 @@ def main():
     pogba_pass_completion_rate = pogba_row["Pass Completion Rate"][0]
 
     shortlist_df = players_df.filter(
-        (pl.col("Pogba Similarity (Cosine)") >= 90)
+        (pl.col("Pogba Similarity") >= 90)
         & (pl.col("Chance Creation Rate") >= pogba_chance_creation_rate)
         & (pl.col("Pass Completion Rate") >= pogba_pass_completion_rate)
     )
@@ -193,7 +188,7 @@ def main():
             "UID",
             "Name",
             "Club",
-            "Pogba Similarity (Cosine)",
+            "Pogba Similarity",
             "Chance Creation Rate",
             "Pass Completion Rate",
         ]
