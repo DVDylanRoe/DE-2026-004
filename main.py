82834aaf799a3a32b1a5edcfb20174d441912038
cowabunga <d_roe@hotmail.co.uk>
2026-04-24 18:23:25 +0100

    Refactor HTML parsing functions: separate read_html and parse_html_table for improved clarity and structure

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from bs4 import BeautifulSoup
 import polars as pl
 
+def read_html(file_path: str):
+    with open(file_path, encoding="utf-8") as file:
+        html = file.read()
+        return html
+    
+def parse_html_table(html: str) -> tuple[list[str], list[list[str]]]:
+    soup = BeautifulSoup(html, "html.parser")
+    table = soup.find("table")
+
+    headers = [header.text for header in table.select("tr th")]
+
+    rows = [
+        [data.text for data in row.find_all("td")]
+        for row in table.select("tr + tr")
+    ]
+    return headers, rows
+
+
 
 def get_players_data(file_path: str) -> pl.DataFrame:
 
-    with open(file_path, encoding="utf-8") as file:
-        html = file.read()
+    html = read_html(file_path)
 
-    soup = BeautifulSoup(html, "html.parser")
-    players_table = soup.find("table")
-
-    players_table_headers = [th.text for th in players_table.select("tr th")]
-
-    players_table_rows = [
-        [td.text for td in row.find_all("td")]
-        for row in players_table.select("tr + tr")
-    ]
+    players_table_headers, players_table_rows = parse_html_table(html)
 
     players_df = pl.DataFrame(
         players_table_rows, schema=players_table_headers, orient="row"
